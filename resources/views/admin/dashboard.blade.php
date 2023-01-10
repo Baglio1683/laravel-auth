bb781477d8013766d558c6fd87a4b18f60f7bb82
MyUsername <alberto.baglivi@gmail.com>
2023-01-10 19:33:54 +0100

    Add implementation

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -25,7 +25,7 @@
         {{-- Header --}}
         <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow">
             <div class="row justify-content-between">
-                <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">BoolPress</a>
+                <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">Projects</a>
                 <button class="navbar-toggler position-absolute d-md-none collapsed" type="button"
                     data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu"
                     aria-expanded="false" aria-label="Toggle navigation">
@@ -61,10 +61,10 @@
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-white {{ Route::currentRouteName() === 'admin.posts.index' ? 'bg-secondary' : '' }}"
-                                    href="{{ route('admin.posts.index') }}">
+                                <a class="nav-link text-white {{ Route::currentRouteName() === 'admin.projects.index' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.projects.index') }}">
                                     <i class="fa-solid fa-list"></i>
-                                    Posts
+                                    Projects
                                 </a>
                             </li>
                         </ul>
