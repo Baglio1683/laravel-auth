673cf35dfce82349284638413a8f7762937b0a05
MyUsername <alberto.baglivi@gmail.com>
2023-01-10 16:20:27 +0100

    move ProjectController on Admin folder and change the namespace

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Admin\Controllers;
 
 use App\Models\Project;
+use App\Http\Controllers\Controller; 
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
 
