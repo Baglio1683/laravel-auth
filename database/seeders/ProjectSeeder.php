c369a97b85689d425f1440af79f55064f10009a5
MyUsername <alberto.baglivi@gmail.com>
2023-01-10 17:26:10 +0100

    Implement and run the seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,16 +4,19 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project; 
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
+    public function run(Faker $faker) {
+        for ($i = 0; $i < 10; $i++) {
+            $project = new Project();
+            $project->title = $faker->sentence(3);
+            $project->content = $faker->text(400);
+            $project->slug = Str::slug($project->title, '-');
+            $project->save();
+        }
     }
 }
