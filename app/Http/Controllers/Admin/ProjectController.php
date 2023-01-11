068e219eb2b719200967f61c08ccdf57b458b7c9
MyUsername <alberto.baglivi@gmail.com>
2023-01-11 18:01:15 +0100

    implement edit crud , without validation rules

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -78,7 +78,10 @@ class ProjectController extends Controller
      */
     public function update(UpdateProjectRequest $request, Project $project)
     {
-        //
+        $form_data = $request->all();
+        $form_data['slug'] = Project::generateSlug($form_data['title']);
+        $project->update($form_data); 
+        return redirect()->route('admin.projects.index')->with('message', '$project->title è stato aggiornato correttamente'); 
     }
 
     /**
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -4,17 +4,22 @@
         <h2 class="text-center">Modifica {{ $project->title }}</h2>
         <div class="row justify-content-center">
             <div class="col-8">
-                <div class="form-group">
-                    <label for="title">Titolo</label>
-                    <input type="text" id="title" name="title" class="form-control" value="{{ $project->title }}">
-                </div>
+                <form action="route('admin.projects.update', $project->slug)" method="POST">
+                    @method('PUT')
+                    @csrf
+                    <div class="form-group">
+                        <label for="title">Titolo</label>
+                        <input type="text" id="title" name="title" class="form-control"
+                            value="{{ $project->title }}">
+                    </div>
 
-                <div class="form-group">
-                    <label for="content">Contenuto</label>
-                    <textarea name="content" id="content" rows="10" class="form-control"> {{ $project->content }}</textarea>
-                </div>
+                    <div class="form-group">
+                        <label for="content">Contenuto</label>
+                        <textarea name="content" id="content" rows="10" class="form-control"> {{ $project->content }}</textarea>
+                    </div>
 
-                <button class="btn btn-warning">Salva</button>
+                    <button type="submit" class="btn btn-warning">Salva</button>
+                </form>
             </div>
         </div>
     </div>
